545c5fabb17619773c386935214456e14b4e8ed5
Christophe Guillon <christophe.guillon@inria.fr>
2026-05-11 03:46:28 +0200

    Add aidge export compile

--- build_binder.sh
+++ build_binder.sh
@@ -7,8 +7,9 @@ tmpdir=tmp.docker_build
 rm -rf $tmpdir
 mkdir -p $tmpdir
 mkdir -p $tmpdir/.binder $tmpdir/tutorials
-cp -a .binder/requirements.txt $tmpdir/.binder
 cp -a .binder/entrypoint.sh $tmpdir/.binder/
+cp -a .binder/*requirements.txt $tmpdir/.binder
+cp -a .binder/*.patch $tmpdir/.binder/
 cp -a tutorials/*.py  tutorials/*.ipynb tutorials/*.md $tmpdir/tutorials
 
 docker build -f .binder/Dockerfile -t xtc-aidge-tutorial-cpsiot $tmpdir
